67dac3614b97b190a1fed7f5a4bfece31c5d8a8b
Chung-Hao Yu <allen.yu@berkeley.edu>
2015-05-05 20:29:19 -0700

    pushing updated NPTSP file

--- NPTSP.py
+++ NPTSP.py
@@ -1,11 +1,13 @@
 
 import os
+from code import NPTSPSolver
 
 
 TEST_PATH = "inputs/"
 
 
 fout = open ("answer.out", "w")
+fout.truncate() # clear file
 inputs = [x for x in os.listdir(TEST_PATH) if os.path.splitext(x)[1] == '.in'] #extract inputs that end with .in
 T = len(inputs) # number of test cases
 for inp in inputs:
@@ -13,8 +15,13 @@ for inp in inputs:
     N = int(fin.readline()) # number of vertices
     v = [[] for i in range(N)] # 2d matrix holding vertices' edge weights
     for i in xrange(N):
-        v[[i] = [int(x) for x in fin.readline().split()]
+        v[i] = [int(x) for x in fin.readline().split()]
     c = fin.readline() # holds color string
+    
+    nptsp_solver = NPTSPSolver(N, v, c)
+    mst_answer = nptsp_solver.findMST()
+    fout.write(str(sum(mst_answer)))
+    fout.write("\n")
 
     # find an answer, and put into assign
     #assign = [0] * N
